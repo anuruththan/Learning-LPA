bbec8897db3e45753e16f6d43e2ae9654408027d
anuruththan <anuruth-se19003@stu.kln.ac.lk>
2025-12-27 01:58:15 +0530

    new properties has been added

--- src/main/java/com/example/learning_jpa/config/SecurityConfig.java
+++ src/main/java/com/example/learning_jpa/config/SecurityConfig.java
@@ -2,12 +2,6 @@ package com.example.learning_jpa.config;
 
 
 import com.example.learning_jpa.util.JwtAuthenticationFilterUtil;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.io.IOException;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
